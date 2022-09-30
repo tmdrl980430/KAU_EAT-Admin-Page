844b9bcc6b3dacee1c1bc8025f172ed7412ae6a4
tmdrl980430 <tmdrl980430@naver.com>
2022-09-30 17:27:31 +0900

    식권 조회 끝

--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -97,18 +97,18 @@ const Dashboard = () => {
     value4: 0,
   }
   let tempMonth = '0'
-  const thisYearTicketUse = [
-    { title: '6월', value1: 34, value2: 78, value3: 30, value4: 40 },
-    { title: '8월', value1: 34, value2: 100, value3: 30, value4: 100 },
-    { title: '9월', value1: 34, value2: 100, value3: 30, value4: 100 },
-  ]
-  // var temp = []
-  // let listCount = 0
-  // let listCount2 = 0
-  const [temp, setTemp] = useState([])
+
+  let temp = []
+  const [value1Sum, setValue1Sum] = useState(0)
+  const [value2Sum, setValue2Sum] = useState(0)
+  const [value3Sum, setValue3Sum] = useState(0)
+  const [value4Sum, setValue4Sum] = useState(0)
   useEffect(() => {
-    console.log(`groupByMonthList :  ${JSON.stringify(groupByMonthList)}`)
-  }, [groupByMonthList])
+    let arr = [1, 2, 3]
+    console.log(`arr push 전: ${arr}`)
+    arr.push(4)
+    console.log(`arr push 후: ${arr}`)
+  }, [])
 
   const getTickets = async () => {
     console.log('getTickets')
@@ -147,8 +147,9 @@ const Dashboard = () => {
             console.log(
               `response.data.result.groupByMonth.length :  ${response.data.result.groupByMonth.length}`,
             )
+            temp = []
             for (let i = 0; i < response.data.result.groupByMonth.length; i++) {
-              tempMonth = response.data.result.groupByMonth[i].month
+              console.log(`temp for문 시작 :  ${JSON.stringify(temp)}`)
               basicObject.title = `${response.data.result.groupByMonth[i].month}월`
               if (response.data.result.groupByMonth[i].mealTypeIdx === 1) {
                 basicObject.value1 = response.data.result.groupByMonth[i].count
@@ -159,18 +160,50 @@ const Dashboard = () => {
               } else if (response.data.result.groupByMonth[i].mealTypeIdx === 4) {
                 basicObject.value4 = response.data.result.groupByMonth[i].count
               }
+              console.log(`basicObject if문 끝나고 :  ${JSON.stringify(basicObject)}`)
+              console.log(`temp if문 끝나고 :  ${JSON.stringify(temp)}`)
               if (
                 i < response.data.result.groupByMonth.length - 1 &&
                 response.data.result.groupByMonth[i].month !=
                   response.data.result.groupByMonth[i + 1].month
               ) {
-                temp.push(basicObject)
-                console.log(`temp :  ${JSON.stringify(temp)}`)
+                console.log(`temp push 전 :  ${JSON.stringify(temp)}`)
+                console.log(`count위 :  ${i}`)
+                console.log(`basicObject :  ${JSON.stringify(basicObject)}`)
+                temp.push({
+                  title: basicObject.title,
+                  value1: basicObject.value1,
+                  value2: basicObject.value2,
+                  value3: basicObject.value3,
+                  value4: basicObject.value4,
+                })
+
+                setValue1Sum(value1Sum + basicObject.value1)
+                setValue2Sum(value2Sum + basicObject.value2)
+                setValue3Sum(value3Sum + basicObject.value3)
+                setValue4Sum(value4Sum + basicObject.value4)
+
+                console.log(`temp push 후:  ${JSON.stringify(temp)}`)
                 console.log(`groupByMonthList :  ${JSON.stringify(groupByMonthList)}`)
               } else if (i == response.data.result.groupByMonth.length - 1) {
-                temp.push(basicObject)
-                console.log(`temp :  ${JSON.stringify(temp)}`)
+                console.log(`temp push 전 :  ${JSON.stringify(temp)}`)
+                console.log(`count아래 :  ${i}`)
+                console.log(`basicObject :  ${JSON.stringify(basicObject)}`)
+                temp.push({
+                  title: basicObject.title,
+                  value1: basicObject.value1,
+                  value2: basicObject.value2,
+                  value3: basicObject.value3,
+                  value4: basicObject.value4,
+                })
+                setValue1Sum(value1Sum + basicObject.value1)
+                setValue2Sum(value2Sum + basicObject.value2)
+                setValue3Sum(value3Sum + basicObject.value3)
+                setValue4Sum(value4Sum + basicObject.value4)
+                console.log(`temp push 후:  ${JSON.stringify(temp)}`)
                 console.log(`groupByMonthList :  ${JSON.stringify(groupByMonthList)}`)
+              } else {
+                console.log(`count if문 안지났을때 :  ${i}`)
               }
             }
             setGroupByMonthList([...temp])
@@ -228,13 +261,13 @@ const Dashboard = () => {
                     <CCol sm={6}>
                       <div className="border-start border-start-4 border-start-success py-1 px-3">
                         <div className="text-medium-emphasis small">조식</div>
-                        <div className="fs-5 fw-semibold">9,123</div>
+                        <div className="fs-5 fw-semibold">{value1Sum}</div>
                       </div>
                     </CCol>
                     <CCol sm={6}>
                       <div className="border-start border-start-4 border-start-info py-1 px-3 mb-3">
                         <div className="text-medium-emphasis small">중식 | 일품</div>
-                        <div className="fs-5 fw-semibold">22,643</div>
+                        <div className="fs-5 fw-semibold">{value2Sum}</div>
                       </div>
                     </CCol>
                   </CRow>
@@ -242,18 +275,18 @@ const Dashboard = () => {
                     <CCol sm={6}>
                       <div className="border-start border-start-4 border-start-warning py-1 px-3 mb-3">
                         <div className="text-medium-emphasis small">중식 | 한식</div>
-                        <div className="fs-5 fw-semibold">78,623</div>
+                        <div className="fs-5 fw-semibold">{value3Sum}</div>
                       </div>
                     </CCol>
                     <CCol sm={6}>
                       <div className="border-start border-start-4 border-start-danger py-1 px-3 mb-3">
                         <div className="text-medium-emphasis small">석식</div>
-                        <div className="fs-5 fw-semibold">49,123</div>
+                        <div className="fs-5 fw-semibold">{value4Sum}</div>
                       </div>
                     </CCol>
                   </CRow>
                   <hr className="mt-0" />
-                  {thisYearTicketUse.map((item, index) => (
+                  {groupByMonthList.map((item, index) => (
                     <div className="progress-group mb-4" key={index}>
                       <div className="progress-group-prepend">
                         <span className="text-medium-emphasis small">{item.title}</span>
