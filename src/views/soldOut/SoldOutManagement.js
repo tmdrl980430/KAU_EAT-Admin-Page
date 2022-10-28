3845e80771a3023c79041df0d1df153c48e6fdab
Duke(김승기) <74481440+tmdrl980430@users.noreply.github.com>
2022-10-28 21:07:15 +0900

    품절등록 완료

--- src/views/soldOut/SoldOutManagement.js
+++ src/views/soldOut/SoldOutManagement.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react'
-import { CButton, CCol, CForm, CFormCheck, CFormInput, CFormLabel, CRow } from '@coreui/react'
+import { CButton, CForm, CFormLabel, CRow } from '@coreui/react'
 import { useRecoilState } from 'recoil'
 import { jwtRecoilState, severURLRecoilState } from 'src/recoil'
 import axios from 'axios'
+import Select from 'react-select'
 
 const SoldOutManagement = () => {
   const now = new Date()
@@ -15,6 +16,79 @@ const SoldOutManagement = () => {
   const [error, setError] = useState(null)
   const [IP, setIP] = useRecoilState(severURLRecoilState)
 
+  const [choice, setChoice] = useState(0)
+
+  const onChange = (value) => {}
+
+  const options = [
+    { value: 'ACTIVE', label: '정상 운영' },
+    { value: 'SOLDOUT', label: '품절' },
+  ]
+  const [breakfastMenuStatus, setBreakfastMenuStatus] = useState('ACTIVE')
+  const [lunchMenuStatus, setLunchMenuStatus] = useState('ACTIVE')
+  const [lunchKoreaMenuStatus, setLunchKoreaMenuStatus] = useState('ACTIVE')
+  const [dinnerMenuStatus, setDinnerMenuStatus] = useState('ACTIVE')
+
+  useEffect(() => {
+    getDateMealTable()
+  }, [])
+
+  useEffect(() => {
+    console.log(`breakfastMenuStatus : ${breakfastMenuStatus}`)
+    console.log(`lunchMenuStatus : ${lunchMenuStatus}`)
+
+    console.log(`lunchKoreaMenuStatus : ${lunchKoreaMenuStatus}`)
+    console.log(`dinnerMenuStatus : ${dinnerMenuStatus}`)
+  }, [breakfastMenuStatus, lunchMenuStatus, lunchKoreaMenuStatus, dinnerMenuStatus])
+
+  const getDateMealTable = async () => {
+    console.log('getDateMealTable')
+    setLoading(true)
+
+    if (today != '') {
+      try {
+        // 요청이 시작 할 때에는 error 와 users 를 초기화하고
+        setError(null)
+        console.log('getDateMealTable_try')
+        // loading 상태를 true 로 바꿉니다.
+        setLoading(true)
+
+        // axios     .defaults     .headers     .common['x-access-token'] = jwt
+
+        const response = await axios
+          .get(`${IP}/menus?date=${today}`, {
+            headers: {
+              'x-access-token': jwt,
+            },
+          })
+          .then((response) => {
+            console.log(`response 확인 : ${response.data.code}`)
+            console.log(`response 확인 : ${response.data}`)
+            console.log(
+              `response.data.result.menus.size 확인 : ${response.data.result.menus.length}`,
+            )
+
+            if (response.data.code === 1000) {
+              setBreakfastMenuStatus(response.data.result.menus[0].menuStatus)
+              setLunchMenuStatus(response.data.result.menus[1].menuStatus)
+              setLunchKoreaMenuStatus(response.data.result.menus[2].menuStatus)
+              setDinnerMenuStatus(response.data.result.menus[3].menuStatus)
+            }
+          })
+          .catch((error) => {
+            console.log(error)
+          })
+        // 데이터는 response.data.code 안에 들어있다. console.log(response.data.result);
+      } catch (e) {
+        console.log('getDateMealTable_catch')
+        console.log(e)
+        setError(e)
+      }
+    }
+    setLoading(false)
+    // loading 끄기
+  }
+
   const [jwt, setJwt] = useRecoilState(jwtRecoilState)
   const today =
     String(koreaNow.getFullYear()) +
@@ -24,13 +98,13 @@ const SoldOutManagement = () => {
     String(koreaNow.getDate()).padStart(2, '0')
 
   const patchSoldOut = async () => {
-    console.log('autoLogin')
+    console.log('patchSoldOut')
 
     setLoading(true)
     try {
       // 요청이 시작 할 때에는 error 와 users 를 초기화하고
       setError(null)
-      console.log('autoLogin_try')
+      console.log('patchSoldOut_try')
       // loading 상태를 true 로 바꿉니다.
       setLoading(true)
 
@@ -38,8 +112,25 @@ const SoldOutManagement = () => {
         .patch(
           `${IP}/menus/status`,
           {
-            mealTypeIds: '',
             date: today,
+            mealTypeAndStatus: [
+              {
+                mealTypeIdx: 1,
+                menuStatus: breakfastMenuStatus,
+              },
+              {
+                mealTypeIdx: 2,
+                menuStatus: lunchMenuStatus,
+              },
+              {
+                mealTypeIdx: 3,
+                menuStatus: lunchKoreaMenuStatus,
+              },
+              {
+                mealTypeIdx: 4,
+                menuStatus: dinnerMenuStatus,
+              },
+            ],
           },
           {
             headers: {
@@ -50,9 +141,8 @@ const SoldOutManagement = () => {
         .then((response) => {
           console.log(`response code확인`, response)
 
-          if (response.data.code === 1001) {
-            console.log('자동 로그인 완료')
-            setJwt(jwt)
+          if (response.data.code === 1000) {
+            console.log('품절 등록 완료')
           }
         })
         .catch((error) => {
@@ -60,7 +150,7 @@ const SoldOutManagement = () => {
         })
       // 데이터는 response.data.code 안에 들어있다. console.log(response.data.result);
     } catch (e) {
-      console.log('autoLogin_catch')
+      console.log('patchSoldOut_catch')
       console.log(e)
       setError(e)
     }
@@ -80,50 +170,89 @@ const SoldOutManagement = () => {
           <CFormLabel htmlFor="inputMenu" className="col-sm-2 col-form-label">
             조식
           </CFormLabel>
-          <CFormCheck
-            value={true}
-            type="radio"
-            name="Breakfast"
-            id="Breakfast"
-            label="정상 운영"
-            defaultChecked
-          />
-          <CFormCheck
-            value={false}
-            type="radio"
-            name="Breakfast"
-            id="BreakfastSoldout"
-            label="품절"
+          <Select
+            styles={{
+              // zIndex
+              menu: (provided) => ({ ...provided, zIndex: 999 }),
+            }}
+            value={options.find((op) => {
+              // choice state에 따라 디폴트 option 세팅
+              return op.value === breakfastMenuStatus
+            })}
+            defaultValue={breakfastMenuStatus}
+            onChange={(value) => {
+              onChange(value.value)
+              setBreakfastMenuStatus(value.value)
+            }}
+            options={options}
           />
         </CRow>
         <CRow className="mb-3">
           <CFormLabel htmlFor="inputMenu" className="col-sm-2 col-form-label">
             중식 | 일품
           </CFormLabel>
-          <CFormCheck type="radio" name="Lunch" id="Lunch" label="정상 운영" defaultChecked />
-          <CFormCheck type="radio" name="Lunch" id="LunchSoldout" label="품절" />
+          <Select
+            styles={{
+              // zIndex
+              menu: (provided) => ({ ...provided, zIndex: 999 }),
+            }}
+            value={options.find((op) => {
+              // choice state에 따라 디폴트 option 세팅
+              return op.value === lunchMenuStatus
+            })}
+            defaultValue={lunchMenuStatus}
+            onChange={(value) => {
+              onChange(value.value)
+              setLunchMenuStatus(value.value)
+            }}
+            options={options}
+          />
         </CRow>
         <CRow className="mb-3">
           <CFormLabel htmlFor="inputMenu" className="col-sm-2 col-form-label">
             중식 | 한식
           </CFormLabel>
-          <CFormCheck
-            type="radio"
-            name="LunchKorea"
-            id="LunchKorea"
-            label="정상 운영"
-            defaultChecked
+          <Select
+            styles={{
+              // zIndex
+              menu: (provided) => ({ ...provided, zIndex: 999 }),
+            }}
+            value={options.find((op) => {
+              // choice state에 따라 디폴트 option 세팅
+              return op.value === lunchKoreaMenuStatus
+            })}
+            defaultValue={lunchKoreaMenuStatus}
+            onChange={(value) => {
+              onChange(value.value)
+              setLunchKoreaMenuStatus(value.value)
+            }}
+            options={options}
           />
-          <CFormCheck type="radio" name="LunchKorea" id="LunchKoreaSoldout" label="품절" />
         </CRow>
         <CRow className="mb-3">
           <CFormLabel htmlFor="inputMenu" className="col-sm-2 col-form-label">
             석식
           </CFormLabel>
-          <CFormCheck type="radio" name="Dinner" id="Dinner" label="정상 운영" defaultChecked />
-          <CFormCheck type="radio" name="Dinner" id="DinnerSoldout" label="품절" />
+          <Select
+            styles={{
+              // zIndex
+              menu: (provided) => ({ ...provided, zIndex: 999 }),
+            }}
+            value={options.find((op) => {
+              // choice state에 따라 디폴트 option 세팅
+              return op.value === dinnerMenuStatus
+            })}
+            defaultValue={dinnerMenuStatus}
+            onChange={(value) => {
+              onChange(value.value)
+              setDinnerMenuStatus(value.value)
+            }}
+            options={options}
+          />
         </CRow>
-        <CButton type="submit">등록하기</CButton>
+        <CButton type="submit" onClick={patchSoldOut}>
+          등록하기
+        </CButton>
       </CForm>
     </>
   )
